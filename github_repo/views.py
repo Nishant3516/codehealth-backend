578d49cf16c5cb3b3bbe96ced0255b46ea64804b
Nishant <bansalnishant3516@gmail.com>
2026-03-29 16:18:48 +0530

    Fix get user repos issue

--- github_repo/views.py
+++ github_repo/views.py
@@ -1,19 +1,21 @@
 import requests
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def get_user_repos(request):
     access_token = request.headers.get("Authorization")
     if not access_token:
         return Response({"error": "No access token provided"}, status=400)
 
     url = "https://api.github.com/user/repos"
-
+    github_token = request.user.githubprofile.github_token
     headers = {
-        "Authorization": f"Bearer {access_token}",
+        "Authorization": f"Bearer {github_token}",
         "Accept": "application/vnd.github.v3+json",
     }
 
